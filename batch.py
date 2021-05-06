f0a64c58b8958b2f7ca732cf3f0bd7fe0f7b2ab0
Arpan Das <dasarpan007@gmail.com>
2021-05-06 09:32:59 -0400

    Create batch.py

---
+++ batch.py
@@ -0,0 +1,5 @@
+import os
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/mass.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/mmax.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/ncol.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/lagrange.py")
