3dcbc98884b365c134943faa9e9e22bacf6fba66
Arpan Das <dasarpan007@gmail.com>
2021-05-06 09:37:47 -0400

    Update batch.py

--- batch.py
+++ batch.py
@@ -1,5 +1,13 @@
 import os
+#change the filepath to your directory
 os.system("python /home/adas45/projects/def-basu/adas45/results/plot/mass.py")
 os.system("python /home/adas45/projects/def-basu/adas45/results/plot/mmax.py")
 os.system("python /home/adas45/projects/def-basu/adas45/results/plot/ncol.py")
 os.system("python /home/adas45/projects/def-basu/adas45/results/plot/lagrange.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/average.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/collision.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/coredensity.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/coreradius.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/energy.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/maxacc.py")
+os.system("python /home/adas45/projects/def-basu/adas45/results/plot/sigma.py")
